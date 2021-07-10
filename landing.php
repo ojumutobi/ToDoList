8b574a4d79cac0907fa6d0d7bff9b2e942d9baaf
ojumutobi <Oloruntobi.Ojumu@DANGOTE.COM>
2021-07-11 00:14:47 +0100

    Com1

--- landing.php
+++ landing.php
@@ -32,7 +32,7 @@
 	<div class="caption">ToDo List Application</div>
 
 
-	<div class="auth_panel">
+	<!--<div class="auth_panel">
 		<form action="login.php" method ="POST">
 			<input type="email" name="email" required placeholder="Email"> 
 			<input type="password" name="pwd" required placeholder="Password"><br>			
@@ -40,6 +40,68 @@
 		</form>	
 
 	</div>
+
+	-->
 	
+	<div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Task Details</h2>
+                        
+                    </div>
+                    <?php
+                    // Include config file
+                   # require_once "config.php";
+                    
+					include('config/db_cxn.php');
+
+                    // Attempt select query execution
+                    $sql = 'SELECT * FROM tasklist';
+                    if ($result = mysqli_query($conn, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo '<table class="table table-bordered table-striped">';
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>Task Description</th>";
+                                        echo "<th>Task Location</th>";
+                                        echo "<th>Salary</th>";
+                                        echo "<th>Action</th>";
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['Id'] . "</td>";
+                                        echo "<td>" . $row['TaskName'] . "</td>";
+                                        echo "<td>" . $row['LocationOfTask'] . "</td>";
+                                        echo "<td>" . $row['Action'] . "</td>";
+                                        echo "<td>";
+                                            echo '<a href="read.php?id='. $row['Id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                            echo '<a href="update.php?id='. $row['Id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                            echo '<a href="delete.php?id='. $row['Id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+                        }
+                    } else{
+                        echo "Oops! Something went wrong. Please try again later.";
+                    }
+ 
+                    // Close connection
+                    mysqli_close($conn);
+                    ?>
+                </div>
+            </div>        
+        </div>
+    </div>
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,11 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">  
 	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<title>ToDo Application</title>
 	<style type="text/css">
 		.brand{
@@ -19,7 +23,11 @@
 			margin:20px auto;
 
 		}
+		.wrapper{
+			width: 600px;
+			margin: 0 auto;
 
+		}
 	</style>
 </head>
 <body class="grey lighten-4">
@@ -30,10 +38,9 @@
 				<li>Total List: 10 <?php  ?></li>
 			</ul>
 			<ul id ="nav-mobile" class="right hide-on-small-and-down">
-				<li><a href="AddList.php" class="btn brand z-depth-0">Create a Task</a>  </li>
+				<li><a href="AddList.php" class="btn brand z-depth-0 fa fa-plus"> Create a Task</a>  </li>
+				<li><a href="Landing.php" class="btn brand z-depth-0 fa fa-plus"> Home</a>  </li>
 			</ul>
-		</div>
-
-		
+		</div>	
 
 	</nav>
